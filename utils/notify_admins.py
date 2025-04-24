463923cc4b386afa10003682fd5e9369e7842ee7
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2025-04-24 16:05:24 +0500

    chanes

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,14 +1,9 @@
 import logging
-
 from aiogram import Dispatcher
-
 from data.config import ADMINS
 
-
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
-
-        except Exception as err:
-            logging.exception(err)
+    try:
+        await dp.bot.send_message(chat_id=935920479, text="Bot ishga tushdi")
+    except Exception as err:
+        logging.exception(err)
